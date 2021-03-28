4daf32096227bb1925b7291abc16adf95b056d38
Davi Petris <davispetris@gmail.com>
2021-03-27 21:10:01 -0300

    execute single cmd funcionando

---
+++ lib/executers.h
@@ -0,0 +1,19 @@
+/**
+* This library is for executing commands
+**/
+#include "../lib/parser.h"
+
+#ifndef _VSH_EXECUTERS_H_
+#define _VSH_EXECUTERS_H_
+
+int execSingle(command_t *command);
+
+int execPiped(commandLine_t *commands);
+
+int execBultin(command_t *command);
+
+int execCommandLine(commandLine_t *commands);
+
+int commandCheck(command_t *command);
+
+#endif
--- src/vsh.c
+++ src/vsh.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include "../lib/parser.h"
+#include "../lib/executers.h"
 
 int main(int argc, char* argv[]) {
     while (1) {
-        printf("vsh> ");
+        printf("\nvsh> ");
         commandLine_t* commandLine = parseLine();
 
-
-
-        printf("Parsed command(s):\n");
-        for (int i = 0; i < commandLine->commandc; i++) {
-            printf("%s:", commandLine->command[i]->commandName);
-            for (int j = 0; j < commandLine->command[i]->argc; j++) {
-                printf("[%s]", commandLine->command[i]->argument[j]);
-            }
-        }
+        execCommandLine(commandLine);
     }
     return 0;
 }
