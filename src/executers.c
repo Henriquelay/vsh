484ef7d8300ba16a6476f9871dba1cec2cf396b0
Bruno Gomes <48951617+brunogomes98@users.noreply.github.com>
2021-04-04 22:21:24 -0300

    Add files via upload

--- src/executers.c
+++ src/executers.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "../lib/executers.h"
 
 int commandCheck(command_t *command){
@@ -63,7 +63,7 @@ int execSingle(command_t *command){
 
     if(!commandCheck(command)){
         printf("\nParsed command doesn't exists!");
-        return 1;
+        exit(1);
     }
 
     int returnValue = execBultin(command);
@@ -75,18 +75,128 @@ int execSingle(command_t *command){
 
     if(pid == -1){
         printf("Failed to fork. Command: %s", command->commandName);
-        return 1;
+        exit(1);
     }else if (pid == 0){
         if(execvp(command->commandName, command->argument) < 0){
             printf("\nCould not execute command: %s", command->commandName);
         }
         exit(0);
     }else{
-        wait(NULL);
+        waitpid(pid, NULL, 0);
         return 0;
     }    
 }
 
+
+// funcionamento dos pipes dessa função:
+// write do child vai no read do parent (usando o pipe child_to_parent)
+// write do parent vai no read do child (usando o pipe parent_to_child)
 int execPiped(commandLine_t *commands){
+
+    int child_to_parent[2];
+    int parent_to_child[2];
+
+    if(pipe(child_to_parent) == -1){
+        perror("\nError on pipe creation: child_to_parent");
+        exit(1);
+    }
+
+    if(pipe(parent_to_child) == -1){
+        perror("\nError on pipe creation: parent_to_child");
+        exit(1);
+    }
+
+    pid_t pids[commands->commandc];
+
+    pid_t mother = fork();
+
+    if(mother < 0){
+        printf("\nFailed to fork Mother process");
+        exit(1);
+    }else if(mother == 0){
+        if(setsid() < 0){
+            printf("sid error");
+            exit(1);
+        }
+
+        for(int i=0; i<commands->commandc; i++){
+
+            if ((i == 0) || (pids[i-1] > 0)){
+                pids[i] = fork();
+
+                if(pids[i] < 0){ // fork fail
+                    printf("Failed to fork. Child: %d", i);
+                    exit(1);
+                }else if(pids[i] == 0){ // child code => execute command
+
+                    command_t *currentCmd = commands->command[i];
+                    if(!commandCheck(currentCmd)){
+                        printf("\nParsed command doesn't exists!");
+                        exit(1);
+                    
+                    }
+
+                    close(parent_to_child[1]);
+                    if(i != 0){
+                        printf("\n%d bind do stdin\n", pids[i]);
+                        dup2(parent_to_child[0], STDIN_FILENO);
+                    }
+                    close(parent_to_child[0]);
+
+
+                    close(child_to_parent[0]);
+                    if ((i+1) != commands->commandc){
+                        printf("\n%d bind do stdout\n", pids[i]);
+                        dup2(child_to_parent[1], STDOUT_FILENO);
+                    }
+                    close(child_to_parent[1]);
+
+                    if(execvp(currentCmd->commandName, currentCmd->argument) < 0){
+                        printf("\nCould not execute command: %s", currentCmd->commandName);
+                    }
+
+                }else{
+                    char buffer[BUFFERSIZE];
+                    int returnValue = 0;
+
+                    if((i+1) != commands->commandc){
+                        returnValue = read(child_to_parent[0], &buffer, BUFFERSIZE);
+                        printf("\n leitura do pipe\n");
+                        printf("\n %s \n", buffer);
+                        if(returnValue < 0){
+                            printf("\nFail to read");
+                            exit(1);
+                        }
+                        returnValue = write(parent_to_child[1], &buffer, strlen(buffer));
+                        printf("\n escrita do pipe\n");
+                        if(returnValue < 0){
+                            printf("\nFail to write");
+                            exit(1);
+                        }
+                    }
+                    /* 
+                    # TODO
+                    Testando com o comando "ls -la | grep vsh": 
+                    - O "ls -la" executa
+                    - o pai recebe a saída de "ls -la"
+                    - repassa a saída pro "grep vsh"
+                    - o "grep vsh" recebe a entrada repassada e printa o resultado no STDOUT do programa
+                    - porém "grep vsh" fica como stopped e não morre, ficando vivo pra sempre em background na sessão do processo mãe
+                    - waitpid(pids[i], NULL, WNOHANG); "resolve" o problema de travamento, porém, ocorre o seguinte:
+                        \_ ./vsh
+                            \_ [ls] <defunct>
+                            \_ grep vsh
+
+                    Pra debuggar tem que pegar o pid da mãe que o programa printa e procurar no comando "ps auxf"
+                    */
+                    waitpid(pids[i], NULL, 0);
+                }
+            }
+        }
+    }else{
+        printf("\nPID da mãe: %d\n", mother);
+        waitpid(mother, NULL, WNOHANG);
+    }
+
     return 0;   
 }
--- src/vsh.c
+++ src/vsh.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "../lib/parser.h"
 #include "../lib/executers.h"
-#include <signal.h>
-#include "../lib/handlerSignal.h"
-
+#include "../lib/handler.h"
 
 int main(int argc, char* argv[]) {
+		struct sigaction act;
+		act.sa_handler = &handler;
+		act.sa_flags = 0;
+		
     while (1) {
-        printf("\nvsh> ");
         commandLine_t* commandLine = parseLine();
 
-        execCommandLine(commandLine);
+        // It frees the memory after execution
+        if(commandLine != NULL) {
+            execCommandLine(commandLine);
+        }
+        commandLine = NULL;
     }
     return 0;
 }
