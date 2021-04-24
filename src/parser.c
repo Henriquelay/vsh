f76287b8cee9d9b04e09342121be221bf7e6ce3b
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2021-04-24 03:15:56 -0300

    Formatting (executers will come on next commit)

--- src/parser.c
+++ src/parser.c
@@ -1,9 +1,9 @@
+#include "../lib/parser.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../lib/parser.h"
 
-void freeCommand(command_t* c) {
+void freeCommand(command_t *c) {
     c->commandName = NULL;
     for (int i = 0; i <= c->argc; i++) {
         free(c->argument[i]);
@@ -12,7 +12,7 @@ void freeCommand(command_t* c) {
     free(c);
 }
 
-void freeCommandLine(commandLine_t* cl) {
+void freeCommandLine(commandLine_t *cl) {
     for (int i = 0; i < cl->commandc; i++) {
         freeCommand(cl->command[i]);
         cl->command[i] = NULL;
@@ -20,8 +20,8 @@ void freeCommandLine(commandLine_t* cl) {
     free(cl);
 }
 
-command_t* initCommand() {
-    command_t* newCommand = (command_t*)malloc(sizeof(command_t));
+command_t *initCommand() {
+    command_t *newCommand = (command_t *)malloc(sizeof(command_t));
     if (newCommand == NULL) {
         perror("Error allocating new command_t");
         exit(EXIT_FAILURE);
@@ -30,8 +30,9 @@ command_t* initCommand() {
     return newCommand;
 }
 
-commandLine_t* initCommandLine() {
-    commandLine_t* newCommandLine = (commandLine_t*)malloc(sizeof(commandLine_t));
+commandLine_t *initCommandLine() {
+    commandLine_t *newCommandLine =
+        (commandLine_t *)malloc(sizeof(commandLine_t));
     if (newCommandLine == NULL) {
         perror("Error allocating new commandLine_t");
         exit(EXIT_FAILURE);
@@ -41,11 +42,11 @@ commandLine_t* initCommandLine() {
 }
 
 // Handy util to create a new string and copy from src using strcpy
-char* copyStr(const char* src) {
+char *copyStr(const char *src) {
     if (src == NULL) {
         return NULL;
     }
-    char* dest = (char*)malloc(sizeof(char) * (strlen(src) + 1));
+    char *dest = (char *)malloc(sizeof(char) * (strlen(src) + 1));
     if (dest == NULL) {
         perror("Error allocating string for copying");
         exit(EXIT_FAILURE);
@@ -56,15 +57,18 @@ char* copyStr(const char* src) {
 }
 
 // Parses a single command from commandLine's strtok
-command_t* parseCommand(char* fullCommand) {
-    command_t* command = initCommand();
+command_t *parseCommand(char *fullCommand) {
+    command_t *command = initCommand();
 
     command->argument[command->argc] = copyStr(strtok(fullCommand, ARGS_DELIM));
     command->commandName = command->argument[command->argc++];
 
-    for (char* currentArg = strtok(NULL, ARGS_DELIM); currentArg != NULL && strcmp(currentArg, COMMAND_DELIM) != 0; currentArg = strtok(NULL, ARGS_DELIM)) {
+    for (char *currentArg = strtok(NULL, ARGS_DELIM);
+         currentArg != NULL && strcmp(currentArg, COMMAND_DELIM) != 0;
+         currentArg = strtok(NULL, ARGS_DELIM)) {
         // printf("Copying str [%s]\n", currentArg);
-        // Just a reminder that arguments[0] is the commandName itself, there there is actually 1 more argument;
+        // Just a reminder that arguments[0] is the commandName itself, there
+        // there is actually 1 more argument;
         if (command->argc > MAX_ARGUMENTS) {
             printf("Too many arguments! Max: %d\n", MAX_ARGUMENTS);
             freeCommand(command);
@@ -77,7 +81,7 @@ command_t* parseCommand(char* fullCommand) {
 }
 
 // Waits and parses line from stdin
-commandLine_t* parseLine(char* wholeLine, size_t lineSize) {
+commandLine_t *parseLine(char *wholeLine, size_t lineSize) {
     // From https://stackoverflow.com/a/27491954/12921102
     do {
         printf("$vsh> ");
@@ -86,16 +90,18 @@ commandLine_t* parseLine(char* wholeLine, size_t lineSize) {
             exit(EXIT_FAILURE);
         };
         size_t len = strlen(wholeLine);
-        if (len > 0 && wholeLine[len-1] == '\n') {
+        if (len > 0 && wholeLine[len - 1] == '\n') {
             wholeLine[--len] = '\0';
         }
     } while (wholeLine[0] == '\0');
 
-    commandLine_t* commandLine = initCommandLine();
+    commandLine_t *commandLine = initCommandLine();
     // TODO build command structure while reading from stdin
     // hint: getdelim
-    char* reentrantPtr;
-    for (char* command = strtok_r(wholeLine, COMMAND_DELIM, &reentrantPtr); command != NULL; command = strtok_r(NULL, COMMAND_DELIM, &reentrantPtr)) {
+    char *reentrantPtr;
+    for (char *command = strtok_r(wholeLine, COMMAND_DELIM, &reentrantPtr);
+         command != NULL;
+         command = strtok_r(NULL, COMMAND_DELIM, &reentrantPtr)) {
         // printf("Parsing command [%s]\n", command);
         if (commandLine->commandc > MAX_COMMANDS) {
             printf("Too many commands! Max: %d\n", MAX_COMMANDS);
@@ -103,7 +109,7 @@ commandLine_t* parseLine(char* wholeLine, size_t lineSize) {
             return NULL;
         }
         // If there was an error parsing arguments
-        command_t* parsedCommand = parseCommand(command);
+        command_t *parsedCommand = parseCommand(command);
         if (parsedCommand == NULL) {
             return NULL;
         }
