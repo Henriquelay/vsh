4107883566f6475eb10a60e3b7f78139ff8ef593
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2021-04-25 01:27:22 -0300

    Refformating

--- lib/parser.h
+++ lib/parser.h
@@ -1,6 +1,7 @@
 /**
-* This library is for parsing commandlines strings and treating user input (not signals)
-**/
+ * This library is for parsing commandlines strings and treating user input (not
+ *signals)
+ **/
 
 #ifndef _VSH_PARSER_H_
 #define _VSH_PARSER_H_
@@ -19,20 +20,21 @@
 
 // Defines a whole command or shell instruction, including arguments
 typedef struct command_t {
-    char* commandName;
-    char* argument[MAX_ARGUMENTS+1];
+    char *commandName;
+    char *argument[MAX_ARGUMENTS + 1];
     // The number of arguments used
     unsigned int argc;
 } command_t;
 
 // Defines a whole shell input line
 typedef struct commandLine_t {
-    command_t* command[MAX_COMMANDS];
+    command_t *command[MAX_COMMANDS];
     // Number of commands used
     unsigned int commandc;
+    // pid_t *pid;
 } commandLine_t;
 
-commandLine_t* parseLine();
+commandLine_t *parseLine();
 
 void freeCommand(command_t *c);
 
